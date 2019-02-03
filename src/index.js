b48394c3003a5b5d7cfa67e73fa69cabd66c841a
MiPadre <elmaquinax@live.com>
2019-02-03 17:39:29 +0100

    Commit configurar handlebars

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@
 //express es una funcion y al ejecutarlo me devuelve un objeto que voy a lmacenarlo en una
 //constante app
 //path es un modulo para ubicar las rutas
+//requerimos el modulo handlebars
 const express = require('express')
 const path = require('path')
+const exphbs = require('express-handlebars')
 
 //INICIALIZACIONES
 const app = express()
@@ -21,6 +23,17 @@ app.set('port', process.env.PORT || 3000)
 //determinado archivo. si ejecuto index.js __dirname me lleva a la carpeta src y la puedo
 //concatenar con otra en este caso views
 app.set('views', path.join(__dirname, 'views'))
+
+//Configuramos el modulo express-handlebars. Utlizamos el metodo engine() de express() y para saber
+// que es un archivo de handlebars ponemos la extension .hbs. al metodo exphbs le damos un objeto
+//de configuracion que tiene propiedades como defaulLayout, layoustDir, partialsDir, extname para
+//saber de que forma vamos a utilizar las vistas
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}))
 
 //MIDDLEWARES: donde iran todas las funciones que van a ser utilizadas antes que lleguen al servidor
 // o de que se pasen a las rutas
