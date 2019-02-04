1a4b35955f1e5fad1265c714d9abc6ddaeb85096
MiPadre <elmaquinax@live.com>
2019-02-04 11:01:10 +0100

    Commit configuracion nodemon

---
+++ src/routes/users.js
@@ -0,0 +1,10 @@
+//Creamos el enrutador. Requerimos express con el metodo Router() que permite manejar y crear las rutas del servidor
+
+const express = require('express')
+const router = express.Router()
+
+/**Cuando visites la pagina principal de mi aplicacion, vas a manejarlo con una funcion que maneje las peticiones y
+las respuestas y vas a enviar un mensaje que diga Index */
+
+
+module.exports = router
