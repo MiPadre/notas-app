cfeb5f37bd9c2e0f7bd1e08133e05ea7d4994642
MiPadre <elmaquinax@live.com>
2019-02-04 11:27:30 +0100

    Commit configurando Index.js y users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,18 @@
-//Creamos el enrutador. Requerimos express con el metodo Router() que permite manejar y crear las rutas del servidor
+/**Creamos el enrutador. Requerimos express con el metodo Router() que permite manejar y crear las rutas del servidor.
+ * users.js va a servir para registrar y autentificar a los usuarios
+ */
 
 const express = require('express')
 const router = express.Router()
 
-/**Cuando visites la pagina principal de mi aplicacion, vas a manejarlo con una funcion que maneje las peticiones y
-las respuestas y vas a enviar un mensaje que diga Index */
+/**Cuando visites la pagina de registro de mi aplicacion, vas a manejarlo con una funcion que maneje las peticiones y
+las respuestas y vas a enviar los siguientes mensajes */
+router.get('/signin', (req, res) => {
+    res.send('Ingresando a la App')
+})
 
+router.get('/signup', (req, res) => {
+    res.send('Formulario de Autentificacion')
+})
 
 module.exports = router
