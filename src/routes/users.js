0058da7ab60d651e895a47246dd9bf491cfedebb
MiPadre <elmaquinax@live.com>
2019-02-05 11:51:36 +0100

    Commit configuracion de signin.hbs y signup.hbs

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,13 +6,13 @@ const express = require('express')
 const router = express.Router()
 
 /**Cuando visites la pagina de registro de mi aplicacion, vas a manejarlo con una funcion que maneje las peticiones y
-las respuestas y vas a enviar los siguientes mensajes */
+las respuestas y vas a enviar los siguientes mensajes. Quitamos los mensajes y renderizamos los archivos */
 router.get('/users/signin', (req, res) => {
-    res.send('Ingresando a la App')
+    res.render('users/signin')
 })
 
 router.get('/users/signup', (req, res) => {
-    res.send('Formulario de Autentificacion')
+    res.render('users/signup')
 })
 
 module.exports = router
