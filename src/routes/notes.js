26cc304bc42759c1db3f55e134008754e80fc03c
MiPadre <elmaquinax@live.com>
2019-02-09 12:52:47 +0100

    Commit manejo de errores en note.js y  new-note.hbs

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -9,11 +9,29 @@ router.get('/notes/add', (req, res) => {
     res.render('notes/new-note')
 })
 
-/**Ruta para recibir datos. El formulario me tiene que eviar a la ruta '/notes/new-note' para poder recibir los datos*/
+/**Ruta para recibir datos. El formulario me tiene que enviar a la ruta '/notes/new-note' para poder recibir los datos.
+ * Le decimos que de la respuesta del body quiero el titulo y la descripcion, y manejamos los errores en el caso de
+ *  que no haya titulo o descripcion, mostrando el tipo de error
+*/
 
 router.post('/notes/new-note', (req, res) => {
-    console.log('req.body')
-    res.send('OK')
+    const {title, description} = req.body
+    const errors = []
+    if(!title){
+        errors.push({text: 'Por favor introduzca un titulo'})
+    }
+    if(!description) {
+        errors.push({text: 'Por favor introduzca una descripcion'})
+    }
+    if(errors.length > 0) {
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+        })
+    }else {
+        res.send('OK')
+    }
 })
 
 /**Cuando visites la pagina notes de mi aplicacion, vas a manejarlo con una funcion que maneje las peticiones y
