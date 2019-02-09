4643f9e363b3165535440f4616fd874c4826ee4e
MiPadre <elmaquinax@live.com>
2019-02-09 11:34:38 +0100

    Commit configuracion de notes.js y new-note.hbs

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -3,6 +3,19 @@
 const express = require('express')
 const router = express.Router()
 
+/**Configuracion de las nuevas notas que introduzcamos */
+
+router.get('/notes/add', (req, res) => {
+    res.render('notes/new-note')
+})
+
+/**Ruta para recibir datos. El formulario me tiene que eviar a la ruta '/notes/new-note' para poder recibir los datos*/
+
+router.post('/notes/new-note', (req, res) => {
+    console.log('req.body')
+    res.send('OK')
+})
+
 /**Cuando visites la pagina notes de mi aplicacion, vas a manejarlo con una funcion que maneje las peticiones y
 las respuestas y vas a enviar de momento un mensaje que diga Notas desde la base de datos. En el futuro devolvera las 
 notas de la base de datos */
